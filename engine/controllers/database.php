b36321559ccc630e60351e44ee0dc27399fedba0
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-08 13:08:25 +0200

    Added debug

--- engine/controllers/database.php
+++ engine/controllers/database.php
@@ -12,7 +12,9 @@ class Database {
             // Set up the connection
             $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";", DB_USER, DB_PASS);
             // Debug
-
+            if(DEBUG) {
+                echo "There is a database connection. ";
+            }
         } catch(PDOException $e){
             // Die if there is an error since the database is a main dependency
             die("There is no database connection " . $e->errorInfo);
