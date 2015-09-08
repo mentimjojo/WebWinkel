f8414c3bc94796dfb4257ea69fe209f5e9e4bd33
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-08 12:28:41 +0200

    New database connection

---
+++ engine/controllers/database/connection.php
@@ -0,0 +1,16 @@
+<?php
+// Class connection
+class Connection{
+
+    private $conn;
+
+    public function ConnectDB(){
+        try{
+            $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";", DB_USER, DB_PASS);
+        } catch(PDOException $e){
+            die("There is no database connection " . $e->errorInfo);
+        }
+    }
+
+}
+?>
