8abc3a3ea95c257486b822ee76dc722fec82162d
Cas de Reuver <casdr@0fi.net>
2015-09-08 13:04:12 +0200

    Debug constant

--- engine/config/constants.php
+++ engine/config/constants.php
@@ -4,3 +4,6 @@ define('DB_USER', $config->db->user);
 define('DB_PASS', $config->db->pass);
 define('DB_HOST', $config->db->host);
 define('DB_NAME', $config->db->name);
+
+// Define debugging
+define('DEBUG', $config->debug);
