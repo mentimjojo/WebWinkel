a5d4f85f1dc73f501756b7093cc29e99f67120f1
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-08 13:53:59 +0200

    Added page system

--- engine/controllers/theme.php
+++ engine/controllers/theme.php
@@ -10,8 +10,23 @@ class Theme{
     }
 
     // Page system
-    public function PageSystem($page){
-
+    public function PageSystem(){
+        // Global config
+        global $config;
+        // Check if page exists
+        if(empty($_GET['page'])){
+            require $config->basedir . '/' . $config->theme->path . '/home.php';
+        } else {
+            // Make page
+            $page = $_GET['page'].'.php';
+            // Check if exits
+            if (file_exists($config->basedir . '/' . $config->theme->path . '/' . $page)) {
+                require $config->basedir . '/' . $config->theme->path . '/' . $page;
+            } else {
+                // Show errors when debug on
+                echo "This page doesn't exists";
+            }
+        }
     }
 
 }
