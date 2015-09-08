03b1dae9f066798013bb1158755f5ce25461439b
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-08 13:14:00 +0200

    Added some comments

--- engine/init.php
+++ engine/init.php
@@ -5,10 +5,14 @@ require_once __DIR__ . '/config/constants.php';
 
 // Enable debugging
 if($config->debug == true) {
+    // Display errors
     ini_set('display_errors', 1);
+    // Enable error reporting
     error_reporting(E_ALL);
 } else {
+    // Don't disable errors
     ini_set('display_errors', 0);
+    // Disable error reporting
     error_reporting(0);
 }
 
@@ -18,11 +22,16 @@ if($config->debug == true) {
  * @return bool
  */
 function autoload($class) {
+    // Make path for classes
     $path = __DIR__ . '/controllers/' . strtolower($class) . '.php';
+    // Check if file exists on path
     if(file_exists($path)) {
+        // Include path/file
         require $path;
+        // Return true if succes
         return true;
     } else {
+        // Return false with error
         return false;
     }
 }
