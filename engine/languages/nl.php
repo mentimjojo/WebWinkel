503f29da14cd53c897f01bad442c8c4fa809a721
Cas de Reuver <casdr@0fi.net>
2015-09-10 09:39:02 +0200

    Dutch language edits

--- engine/languages/nl.php
+++ engine/languages/nl.php
@@ -6,8 +6,26 @@ return (object) array(
             'help' => 'Help',
             'contact' => 'Contact',
             'deliveryinfo' => 'Bezorg informatie',
-            'callus' => 'Bel ons: :phonenumber:'
+            'callus' => 'Bel ons: :phonenumber:',
+            'register' => 'Registreren',
+            'search' => 'Zoeken',
         ),
-        'welcome' => 'Welcome to PC4U'
+        'login' => (object) array(
+            'login' => 'Inloggen',
+            'signin' => 'Inloggen',
+            'email' => 'E-mail adres',
+            'password' => 'Wachtwoord',
+            'remember' => 'Onthoud gegevens',
+            'forgot' => 'Wachtwoord vergeten'
+        ),
+        'cart' => (object) array(
+            'items' => 'producten',
+            'checkout' => 'Bestellen',
+        ),
+        'navbar' => (object) array(
+            'home' => 'Home'
+        ),
+        'welcome' => 'Welkom bij PC4U',
+
     )
 );
