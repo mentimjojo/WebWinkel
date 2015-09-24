7ff5f2162ccf0f5cfc7fe7b23ae7f4a6cba54b70
Tim Nijborg <timnijborg@hotmail.nl>
2015-09-24 12:05:03 +0200

    Changed some things

--- engine/init.php
+++ engine/init.php
@@ -9,13 +9,13 @@ session_start();
 ob_start();
 
 // Check if session is isset
-if(!isset($_SESSION['login_status'])){
+if (!isset($_SESSION['login_status'])) {
     // Set session
     $_SESSION['login_status'] = 0;
 }
 
 // Enable debugging
-if($config->debug->errors == true) {
+if ($config->debug->errors == true) {
     // Display errors
     ini_set('display_errors', 1);
     // Enable error reporting
@@ -32,11 +32,12 @@ if($config->debug->errors == true) {
  * @param $class
  * @return bool
  */
-function autoload($class) {
+function autoload($class)
+{
     // Make path for classes
     $path = __DIR__ . '/controllers/' . strtolower($class) . '.php';
     // Check if file exists on path
-    if(file_exists($path)) {
+    if (file_exists($path)) {
         // Include path/file
         require $path;
         // Return true if succes
@@ -54,14 +55,14 @@ spl_autoload_register('autoload');
 $database = new Database();
 
 // Get user lang
-if(isset($_GET['lang']) OR isset($_COOKIE['lang'])) {
+if (isset($_GET['lang']) OR isset($_COOKIE['lang'])) {
     // Check if lang get is set
-    if(isset($_GET['lang'])){
+    if (isset($_GET['lang'])) {
         // Set cookie
         setcookie('lang', $_GET['lang']);
         // Set lang
         $userlang = $_GET['lang'];
-    } else if(isset($_COOKIE['lang'])) {
+    } else if (isset($_COOKIE['lang'])) {
         // Else set lang
         $userlang = $_COOKIE['lang'];
     }
@@ -78,7 +79,8 @@ $config->language->current = $userlang;
  * Short function for getting a translated string gusing the language class
  * @param string $key
  */
-function lang($key = '', $replace = array()) {
+function lang($key = '', $replace = array())
+{
     global $lang;
     echo $lang->get($key, $replace);
 }
@@ -93,19 +95,19 @@ $mailer = new Mailer();
 $account = new Account();
 
 // Check if user want to logout
-if(!isset($_GET['logout'])){
+if (!isset($_GET['logout'])) {
     // Set logout if is nothing
     $_GET['logout'] = '';
     // Set session nothing
-    if($_SESSION['login_status'] == 0) {
+    if ($_SESSION['login_status'] == 0) {
         // Set hash nothing
         $_SESSION['login_hash'] = '';
     }
-} else if($_GET['logout'] == 1){
+} else if ($_GET['logout'] == 1) {
     // Logoout user
     $account->logoutUser($_SESSION['login_hash']);
     // Send to home page
-    header('location: '.$config->path->basepath.'/home/');
+    header('location: ' . $config->path->basepath . '/home/');
 }
 
 // Create theme object
@@ -114,4 +116,5 @@ $theme = new Theme();
 // Get theme
 $theme->getTheme();
 
+//
 ?>
